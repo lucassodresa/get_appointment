8544a9c07c37005fd18c8fd2c7ae8b9617ddc11d
lucassodresa <lucas.sodresa@gmail.com>
2022-05-24 22:38:15 +0100

    role based content sample

--- web/src/pages/Private/Appointments/index.js
+++ web/src/pages/Private/Appointments/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { useRecoilValue } from 'recoil';
 import { userRoleSelector } from '../../../recoil/user';
 import Page from '../../../shared/Page';
-
+import { GLOBALS } from '@get_appointment/shared';
+const { ROLES } = GLOBALS.USER;
 const Appointments = () => {
   const userRole = useRecoilValue(userRoleSelector);
 
-  console.log(userRole);
+  if (!userRole) return null;
 
-  return <Page title="Appointments"></Page>;
+  return (
+    <Page title="Appointments">
+      {userRole === ROLES.NORMAL && 'You are a Normal user'}
+      {userRole === ROLES.COMPANY && 'You are a Company user'}
+    </Page>
+  );
 };
 
 export default Appointments;
